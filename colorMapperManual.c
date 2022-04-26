42fec134300f93621cf4bf71ed6ca5ad7055f9f2
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-04-27 00:00:21 +0530

    Create colorMapperManual.c

---
+++ colorMapperManual.c
@@ -0,0 +1,17 @@
+#include "colorMapperManual.h"
+
+
+void colorMapperManual()
+{
+ 	int pairNumber=0; 
+    ColorPair colorPair;
+     for(int MajorpairNumber=1; MajorpairNumber<numberOfMajorColors; MajorpairNumber++)
+     {
+        for(int MinorpairNumber=1; MinorpairNumber<numberOfMinorColors*; MinorpairNumber++)
+        {
+            ++pairNumber;
+            colorPair = GetColorFromPairNumber(pairNumber);
+            printf(COLORBLU"PairNumber:"COLORNORMAL"%d----->"COLORRED"MajorColor:"COLORNORMAL "%s""----->"COLORYEL"MinorColor:"COLORNORMAL"%s\n", pairNumber,MajorColorNames[colorPair.majorColor],MinorColorNames[colorPair.minorColor]);
+       }
+     }
+}
