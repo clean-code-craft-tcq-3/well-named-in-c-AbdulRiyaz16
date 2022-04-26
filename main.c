bc664a694f333724ba63eef58b6f2f4035e1a899
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-04-26 23:25:43 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,7 @@
-#include <stdio.h>
-#include <assert.h>
 #include "colorpair.h"
 
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
-{
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
-}
-
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
-void colorMapperManual()
-{
-     int pairNumber=0; 
-     ColorPair colorPair;
-     for(int MajorpairNumber=0; MajorpairNumber<numberOfMajorColors; MajorpairNumber++)
-     {
-        for(int MinorpairNumber=0; MinorpairNumber<numberOfMinorColors; MinorpairNumber++)
-        {
-            ++pairNumber;
-            colorPair = GetColorFromPairNumber(pairNumber);
-            printf(COLORBLU"PairNumber:"COLORNORMAL"%d----->"COLORRED"MajorColor:"COLORNORMAL "%s""----->"COLORYEL"MinorColor:"COLORNORMAL"%s\n", pairNumber,MajorColorNames[colorPair.majorColor],MinorColorNames[colorPair.minorColor]);
-        }
-     }
-}
 int main() {
-    colorMapperManual();
+
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
